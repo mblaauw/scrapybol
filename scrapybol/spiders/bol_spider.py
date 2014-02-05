f0dfc221e4f9d1a0c9ae68243976d0ffc8e02fc5
mblaauw <michel.blaauw@gmail.com>
2014-02-05 13:44:48 +0100

    under construction

--- scrapybol/spiders/bol_spider.py
+++ scrapybol/spiders/bol_spider.py
@@ -1,13 +1,27 @@
+import re
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
 
 
 class BolSpider(BaseSpider):
     name = "bol"
     allowed_domains = ["bol.com"]
-    start_urls = [
-        "http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-nieuw/N/255+8293+14033/No/12/section/books/index.html"
-    ]
+
+    base_url = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-thrillers/N/261+8293/No/0/section/books/index.html'
+    html = urlopen(base_url).read()
+    soup = BeautifulSoup(html, 'lxml')
+
+    total_nr_of_items = re.sub('<[^>]+>', '', str(soup.find('span', 'tst_searchresults_nrFoundItems')))
+    total_nr_of_items = int(round(int(float(total_nr_of_items.replace('.', '')))/12))
+
+    total_nr_of_items = 5
+
+    start_urls = []
+    for eachItem in range(0, total_nr_of_items):
+        new_url = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-thrillers/N/261+8293/No/' + str(eachItem * 12) + '/section/books/index.html'
+        start_urls.append(new_url)
 
 
     def parse(self, response):
@@ -16,44 +30,49 @@ class BolSpider(BaseSpider):
         ratings = []
         sites = hxs.select('//meta[@itemprop="ratingValue"]')
         for site in sites:
-            ratings.append(str(site.select('@content').extract()))
+            ratings.append(str(site.select('@content').extract())[3:-2])
 
         languages = []
         sites = hxs.select('//meta[@itemprop="inLanguage"]')
         for site in sites:
-            languages.append(str(site.select('@content').extract()))
+            languages.append(str(site.select('@content').extract())[3:-2])
 
         pubdates = []
         sites = hxs.select('//meta[@itemprop="datePublished"]')
         for site in sites:
-            pubdates.append(str(site.select('@content').extract()))
+            pubdates.append(str(site.select('@content').extract())[3:-2])
 
         covers = []
         isbns = []
         sites = hxs.select('//img[@itemprop="image"]')
         for site in sites:
-            isbns.append(str(site.select('@src').extract()).split('/')[11][:-6])
-            covers.append(str(site.select('@src').extract()))
+            isbn = str(site.select('@src').extract())
+            isbn = str(isbn[len(isbn)-25:-6])
+
+            # todo: clean the isbn further
+
+            isbns.append(isbn)
+            covers.append(str(site.select('@src').extract())[3:-2])
 
         titles = []
         sites = hxs.select('//a[@itemprop="name"]')
         for site in sites:
-            titles.append(str(site.select('text()').extract()))
+            titles.append(str(site.select('text()').extract())[3:-2])
 
         authors = []
         sites = hxs.select('//a[@itemprop="author"]')
         for site in sites:
-            authors.append(str(site.select('text()').extract()))
+            authors.append(str(site.select('text()').extract())[3:-2])
 
         prices = []
         sites = hxs.select('//strong[@itemprop="price"]')
         for site in sites:
-            prices.append(str(site.select('text()').extract()))
+            prices.append(str(site.select('text()').extract())[10:-2])
 
         ebookprices = []
         sites = hxs.select('//span[@class="pricetag"]')
         for site in sites:
-            ebookprices.append(str(site.select('text()').extract()))
+            ebookprices.append(str(site.select('text()').extract())[10:-2])
 
         bindingcodes = []
         sites = hxs.select('//span[@data-attr-key="BINDINGCODE"]')
